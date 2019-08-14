1292aa9ed673b11441fc0cded4d805b358a90eb2
Rek3dge <samuel.gustafsson@futuregames.nu>
2019-08-14 16:03:16 +0200

    Added Left flipper Began Player Input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    //private
+    Camera playerCamera;
+
+    //public
+
+    void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        playerCamera = Camera.main;
+    }
+    
+    void Update()
+    {
+        
+    }
+}

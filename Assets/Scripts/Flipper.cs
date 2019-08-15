3c5eaea308a0cb85c7b1115c23c9f0bad66bc674
Rek3dge <samuel.gustafsson@futuregames.nu>
2019-08-15 12:41:55 +0200

    Added flipper functionallity Changed Input settings

---
+++ Assets/Scripts/Flipper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Flipper : MonoBehaviour
+{
+    //Public
+    public float flipperSpeed = 1000;
+    public float reverseMod = 1f;
+    [NonSerialized] public bool isFlipped = false;
+
+    //Private
+    HingeJoint2D flipperHinge;
+    
+    void Awake()
+    {
+        flipperHinge = GetComponent<HingeJoint2D>();
+        Assert.IsNotNull(flipperHinge, "Couldn't find the hinge component on " + gameObject.name);
+    }
+    private void FixedUpdate()
+    {
+        if (isFlipped)
+        {
+            JointMotor2D flipperMotor = flipperHinge.motor;
+            flipperMotor.motorSpeed = flipperSpeed;
+
+            flipperHinge.motor = flipperMotor;
+        }
+        else
+        {
+            //TODO: Add reverse mod
+            JointMotor2D flipperMotor = flipperHinge.motor;
+            flipperMotor.motorSpeed = -flipperSpeed;
+
+            flipperHinge.motor = flipperMotor;
+        }
+    }
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,26 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerInput : MonoBehaviour
 {
     //private
     Camera playerCamera;
+    private Flipper leftFlipper;
+    private Flipper rightFlipper;
 
+    const string leftFlipperName = "Left Flipper";
     //public
 
+    #region Unity
     void Awake()
     {
-        
+        playerCamera = Camera.main;
+        leftFlipper = GetFlipper(leftFlipperName);
+        Assert.IsNotNull(leftFlipper, "Could not find child: " + leftFlipperName);
     }
 
-    void Start()
-    {
-        playerCamera = Camera.main;
-    }
-    
     void Update()
     {
-        
+        leftFlipper.isFlipped = Input.GetButton(leftFlipperName);
+    }
+    #endregion
+
+
+    private Flipper GetFlipper(string flipperName)
+    {
+        //Transform flipperTransform = transform.Find(flipperName);
+        //Assert.IsNotNull(flipperTransform, "Could not find flipper with name: " + flipperName);
+        //Flipper flipper = flipperTransform.GetComponent<Flipper>();
+        //Assert.IsNotNull(flipper, "Could not find flipper script in gameObject: " + flipperName);
+
+        return transform.Find(flipperName)?.GetComponent<Flipper>();
     }
 }
